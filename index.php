f0304a66c0524cca2e166746416e5ef032b03e4a
NaufalFiraas <naufalfiraas97@gmail.com>
2020-02-25 15:06:22 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Belajar Git Remote</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sint repudiandae id facere dicta nisi nostrum voluptates laudantium doloribus eius alias iste, sequi nobis voluptatum quibusdam saepe culpa consequuntur debitis! Officiis.</p>
 </body>
 </html>
